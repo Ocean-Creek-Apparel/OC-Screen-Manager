ca09c9795dadf47b4cc3953fdded76fe617e2dd3
Cullen G <gostelcm@dukes.jmu.edu>
2025-07-31 12:18:45 -0400

    Continued refactor

---
+++ Application 2/controller/controller.py
@@ -0,0 +1,14 @@
+class Controller:
+    """
+    This class coordinates the models and views to perform
+    their intended tasks.
+    """
+
+    def delete_screen(id):
+        pass
+
+    def update_screen(id):
+        pass
+    
+    def create_screen():
+        pass
