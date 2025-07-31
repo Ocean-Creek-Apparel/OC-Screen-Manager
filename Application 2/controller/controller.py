e08fe1f55282ea9644ca44d12b96f8b6a5334bdc
Cullen G <gostelcm@dukes.jmu.edu>
2025-07-31 16:04:02 -0400

    Implemented DB operations for screens

--- Application 2/controller/controller.py
+++ Application 2/controller/controller.py
@@ -1,4 +1,6 @@
 import sqlite3
+from model.screen import Screen
+from model.location import Location
 
 class Controller:
     """
@@ -6,20 +8,37 @@ class Controller:
     their intended tasks.
 
     Attributes:
-        connection (Connection): the sqlite3 db connection
+        connection (sqlite3.Connection): the sqlite3 db connection
+        screens (List[Screens]): a list of screens in the database
     """
 
-    def __init__(self, db_path):
+    def __init__(self, db_path: str):
         self.connection = sqlite3.connect(db_path)
+        self.update_screen_list()
 
-    def update_db_path(self, new_path):
+    def update_screen_list(self):
+        """
+        Reads all screens from database and assigns that list to self.screens
+        """
+        self.screens = Screen.read_all(self.connection)
+
+    def update_db_path(self, new_path: str):
         self.connection = sqlite3.connect(new_path)
 
-    def delete_screen(id):
-        pass
+    def add_screen(self, screen: Screen):
+        """
+        Add or update the provided screen's data to the db.
+        
+        Arguments:
+            screen (Screen): the screen to update/add.
+        """
+        success = screen.add_to_db(self.connection)
+        if success:
+            self.update_screen_list()
 
-    def update_screen(id):
-        pass
+    def delete_screen(self, screen):
+        success = screen.delete_from_db(self.connection)
+        if success:
+            self.update_screen_list()
 
-    def create_screen():
-        pass
+    
