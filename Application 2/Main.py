de8163c74eaf9f67701284b6df84b646e5a7dfd9
Cullen G <gostelcm@dukes.jmu.edu>
2025-07-31 16:04:02 -0400

    Implemented DB operations for screens

--- Application 2/Main.py
+++ Application 2/Main.py
@@ -1,15 +1,27 @@
 from model.screen import Screen
+from controller.controller import Controller
 import sqlite3
+import json
+from pathlib import Path
+
+def load_config():
+    """
+    Loads config from the /config/settings.json file.
+    """
+    settings_path = Path(__file__).parent / "config" / "settings.json"
+    with open(settings_path, "r") as f:
+        return json.load(f)
 
 def main():
     """
     The main application loop.
     """
-    conn = sqlite3.Connection(r'C:\Users\cugos\OneDrive\Documents\GitHub\OC-Screen-Manager\Application 2\database\Screen_Database.db')
-    screens = Screen.read_all(conn)
-    print("hello")
-    for screen in screens:
-        print(screen.design)
+    config = load_config()
+    db_path = config["db_path"]
+
+    controller = Controller(db_path)
+    screens = Screen.read_all(controller.connection)
+
 
 if __name__ == "__main__":
     main()
