946d42d9c6c874502868dca48c9ce30af9438ac0
Cullen G <gostelcm@dukes.jmu.edu>
2025-07-31 16:19:02 -0400

    implemented config file functionality

--- Application 2/Main.py
+++ Application 2/Main.py
@@ -2,25 +2,16 @@ from model.screen import Screen
 from controller.controller import Controller
 import sqlite3
 import json
-from pathlib import Path
-
-def load_config():
-    """
-    Loads config from the /config/settings.json file.
-    """
-    settings_path = Path(__file__).parent / "config" / "settings.json"
-    with open(settings_path, "r") as f:
-        return json.load(f)
 
 def main():
     """
     The main application loop.
     """
-    config = load_config()
-    db_path = config["db_path"]
+    controller = Controller()
+    controller.update_db_path(r'C:\Users\cugos\OneDrive\Documents\GitHub\OC-Screen-Manager\Application 2\database\Screen_Database.db')
 
-    controller = Controller(db_path)
-    screens = Screen.read_all(controller.connection)
+    for screen in controller.screens:
+        print(screen.design)
 
 
 if __name__ == "__main__":
