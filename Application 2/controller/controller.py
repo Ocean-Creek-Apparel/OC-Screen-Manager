a3b06290989587360621a35c8bbe11ea9bc2d8c7
Cullen G <gostelcm@dukes.jmu.edu>
2025-07-31 13:03:53 -0400

    continued refactor

--- Application 2/controller/controller.py
+++ Application 2/controller/controller.py
@@ -2,13 +2,19 @@ class Controller:
     """
     This class coordinates the models and views to perform
     their intended tasks.
+
+    Attributes:
+        connection (Connection): the sqlite3 db connection
     """
+
+    def __init__(self, connection):
+        self.connection = connection
 
     def delete_screen(id):
         pass
 
     def update_screen(id):
         pass
-    
+
     def create_screen():
         pass
